cf4b4a970d22525e5692b566cc191068127bb582
marwane2001 <marwane.skaro@e-polytechnique.ma>
2024-12-31 19:04:35 +0200

    app.vue change

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup>
-import Herosection from "@/components/Herosection.vue";
-</script>
-
 <template>
-<Herosection/>
-</template>
-
+    <div class="bg-[#111827] min-h-screen">
+      <NavBar />
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineAsyncComponent } from 'vue';
+  // Import Components Asynchronously
+  const NavBar = defineAsyncComponent(() => import('@/components/NavBar.vue'));
+  </script>
+  
